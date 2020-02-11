c13635c50e0542104b80f551014dfaa1e43f24cc
ermias-1 <vv8702in@go.minnstate.edu>
2020-02-10 22:10:57 -0600

    Changing the name of the form

--- FutureValue/frmFutureValue.Designer.cs
+++ FutureValue/frmFutureValue.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FutureValue
 {
-    partial class Form1
+    partial class frmFutureValue
     {
         /// <summary>
         /// Required designer variable.
--- FutureValue/frmFutureValue.cs
+++ FutureValue/frmFutureValue.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FutureValue
 {
-    public partial class Form1 : Form
+    public partial class frmFutureValue : Form
     {
-        public Form1()
+        public frmFutureValue()
         {
             InitializeComponent();
         }
